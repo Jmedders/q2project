0a949b6840ee80d3465842d41e606c55c1fbc5c9
Josh Newsom <newsommeister@gmail.com>
2016-06-14 15:34:59 -0600

    Member names are passed to band view, but font size is zero

--- routes/bands.js
+++ routes/bands.js
@@ -1,17 +1,47 @@
 var express = require('express'),
-  router = express.Router(),
-  knex = require('../db/knex'),
-  getBandData = require("../temp.js").getBandData; // This is just for getting the mock data.
+    router = express.Router(),
+    knex = require('../db/knex')
+    // getBandData = require("../temp.js").getBandData; // This is just for getting the mock data.
 
 router.get('/', (req, res) => {
+    var bands = [];
 
-    res.render('bands');
+    knex('bands')
+        .then(function(data) {
+            for (var i = 0; i < data.length; i++) {
+                bands.push({
+                    name: data[i].band_name,
+                    band_id: data[i].id
+                });
+            }
+            res.render('bands', { // Render bands page with bands array
+                bands: bands
+            });
+        })
+
 });
 
 router.get('/:band_id', (req, res) => {
-  var data = getBandData(req.params.band_id); // Get data from id.
-  if(/*user is admin of band*/true) data.isAdmin = true;
-  res.render('band', {});
+    // var data = getBandData(req.params.band_id); // Get data from id.
+
+    var members = [];
+
+    knex('bands')
+        .innerJoin('users_bands', 'users_bands.bands_id', 'bands.id')
+        .innerJoin('users', 'users_bands.users_id', 'users.id')
+        .where('bands.id', req.params.band_id)
+        .then(function(data) {
+            for (var i = 0; i < data.length; i++) {
+                members.push({
+                    name: data[i].display_name
+                });
+            }
+            res.render('band', {
+                members: members
+            });
+        });
+
+    // if ( /*user is admin of band*/ true) data.isAdmin = true;
 });
 
 module.exports = router;
