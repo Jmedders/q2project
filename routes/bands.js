4a0d1b7000698aa6983bed1457434e2c6d74fd56
Josh Newsom <newsommeister@gmail.com>
2016-06-16 16:57:27 -0600

    Added Logging

--- routes/bands.js
+++ routes/bands.js
@@ -18,6 +18,7 @@ router.get('/', (req, res) => {
     var magic = Magic(1, ubands => {
       knex('bands')
           .then(function(data) {
+            console.log(data);
             bands = data;
             if(ubands && res.locals.user){
               res.locals.user.bands = ubands;
