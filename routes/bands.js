2b122624feaeb0b1e03d457d8ef7b2b9bc8c5fbd
Josh Newsom <newsommeister@gmail.com>
2016-06-16 17:07:38 -0600

    Checkign stuff

--- routes/bands.js
+++ routes/bands.js
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
                 res.render('bands', { // Render bands page with bands array
                     bands: bands
                 });
-            });
+            }).catch(err => res.render("bands", {bands: bands}));
     });
 
     if (res.locals.user) {
