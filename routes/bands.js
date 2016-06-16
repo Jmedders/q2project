1f903b26a2acf9292357a8cfcc9bdeb3ca9ad85d
Josh Newsom <newsommeister@gmail.com>
2016-06-16 17:02:32 -0600

    Added logging

--- routes/bands.js
+++ routes/bands.js
@@ -4,31 +4,31 @@ var express = require('express'),
     getBandData = require('../getBandData.js').getBandData,
     usersBands = require('../usersBands.js');
 
-function Magic(num, fn){
-  var args = [];
-  return data => {
-    args.push(data);
-    if(args.length === num) fn.apply(null, args);
-  }
+function Magic(num, fn) {
+    var args = [];
+    return data => {
+        args.push(data);
+        if (args.length === num) fn.apply(null, args);
+    }
 }
 
 router.get('/', (req, res) => {
     var bands = [];
 
     var magic = Magic(1, ubands => {
-      knex('bands')
-          .then(function(data) {
-            bands = data;
-            if(ubands && res.locals.user){
-              res.locals.user.bands = ubands;
-              ubands = ubands.map(band => band.band_name);
-              bands = data.filter(band => ubands.indexOf(band.band_name) === -1);
-            }
-              // console.log('bands:', bands);
-              res.render('bands', { // Render bands page with bands array
-                  bands: bands
-              });
-          });
+        knex('bands')
+            .then(function(data) {
+                bands = data;
+                if (ubands && res.locals.user) {
+                    res.locals.user.bands = ubands;
+                    ubands = ubands.map(band => band.band_name);
+                    bands = data.filter(band => ubands.indexOf(band.band_name) === -1);
+                }
+                // console.log('bands:', bands);
+                res.render('bands', { // Render bands page with bands array
+                    bands: bands
+                });
+            });
     });
 
     if (res.locals.user) {
@@ -53,6 +53,7 @@ router.get('/:band_id', (req, res, next) => {
             .innerJoin('users_bands', 'users_id', 'users.id')
             .innerJoin('bands', 'bands_id', 'bands.id')
             .then(data => {
+                console.log('data:', data);
                 res.locals.isAdmin = data.is_admin; // and assign it to res.locals
                 next();
             })
