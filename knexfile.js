740ca06e0b4f9a06e8f4fe21f252dcc11cbe7208
Josh Newsom <newsommeister@gmail.com>
2016-06-17 10:30:11 -0600

    changing something small

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 module.exports = {
     development: {
         client: 'pg',
-        connection: 'postgres://localhost:5432/bandstuff'
+        connection: process.env.DATABASE_URL || 'postgres://localhost:5432/bandstuff'
     },
     production: {
         client: 'pg',
